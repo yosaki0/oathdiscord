9d8aaf75fbef2d8c873c1e79d2b53129aad32c21
0x4eb <77864226+IP37247642654@users.noreply.github.com>
2023-08-01 13:58:55 +0200

    renamed

---
+++ oathdiscord/ext.py
@@ -0,0 +1,104 @@
+from typing import Any, Optional, Type
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.ext.commands.bot import PrefixType, _default
+from discord.ext.commands.help import HelpCommand
+from discord.app_commands import CommandTree
+from discord.ext.commands._types import BotT
+from discord import Intents
+from typing import NewType
+from discord.abc import Snowflake
+import aiohttp
+
+ClientId = NewType("ClientId", str)
+ClientSecret = NewType("ClientSecret", str)
+
+class OauthAppCommandPermissions: 
+    def __init__(self, id: Snowflake, type: int, permission: bool):
+        """
+
+        Args:
+            id (str | int): 
+            type (int): 1(role)  2(user)  3(channel)
+            permission (bool): set the permission on[true] or off[false]
+        """
+        self.id = str(id)
+        if (not type in [1,2,3]): raise ValueError('Invalid value for "type" argument.')
+        self.type = type
+        self.permission = permission
+        
+    def to_dict(self) -> dict:
+        return {'id': self.id, 'type': self.type, 'permission': self.permission} 
+
+class OauthAppConnections:
+    def __init__(self, 
+            type: str = None, 
+            id: str = None, 
+            name: str = None, 
+            visibility: int = None, 
+            friend_sync: bool = None, 
+            show_activity: bool = None, 
+            verified: bool = None, 
+            two_way_link: bool = None, 
+            metadata_visibility: int = None
+        ):
+        self.type = type
+        self.id = id
+        self.name = name
+        self.visibility = visibility
+        self.friend_sync = friend_sync
+        self.show_activity = show_activity
+        self.verified = verified
+        self.two_way_link = two_way_link
+        self.metadata_visibility = metadata_visibility
+    
+    def __repr__(self) -> str:
+        return f"<OauthAppConnections(type={self.type})>"
+
+class Bot(commands.Bot):
+    def __init__(
+        self,
+        command_prefix: PrefixType[BotT],
+        *,
+        help_command: HelpCommand | None = ...,
+        tree_cls: type[CommandTree[Any]] = app_commands.CommandTree,
+        description: str | None = None,
+        intents: Intents,
+        bearer_token: str = None,
+        client_details: tuple[ClientId, ClientSecret] = None,
+        token: str = None,
+        **options: Any,
+    ) -> None:
+        self.bearer_token = bearer_token
+        self.client_id = client_details[0]
+        self.client_secret = client_details[1]
+        self.bot_token = token
+        self._base_url = "https://discord.com/api/v10/applications/%s"
+        self._headers = {}
+        
+        super().__init__(
+            command_prefix,
+            help_command=help_command,
+            tree_cls=tree_cls,
+            description=description,
+            intents=intents,
+            **options
+        )
+    
+    async def edit_app_command_perms(self, guild_id: str, command_id: str, permissions: list[OauthAppCommandPermissions]):
+        url = self._base_url % (f"{self.client_id}/guilds/{guild_id}/commands/{command_id}/permissions")
+        self._headers["Authorization"] = f"Bearer {self.bearer_token}"
+        json_data = {"permissions": []}
+        
+        for perms in permissions:
+            if not isinstance(perms, OauthAppCommandPermissions):
+                raise TypeError("invalid permissions type")
+            json_data["permissions"].append(perms.to_dict())
+            
+        async with aiohttp.ClientSession() as session:
+            request = await session.put(url, headers=self._headers, json=json_data)
+            request_json = await request.json()
+
+        return OauthAppConnections(**request_json) if request.status == 200 else request_json
+        
